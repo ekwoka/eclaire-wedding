c50959e8fbf65aa369a8e8e9cddcfb6cde8c9226
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-07-19 22:11:41 +0400

    :construction: Cleans Repo

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,8 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import { Slot, component$, useStyles$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import type { RequestHandler } from '@builder.io/qwik-city';
 
-import Header from "~/components/starter/header/header";
-import Footer from "~/components/starter/footer/footer";
-
-import styles from "./styles.css?inline";
+import styles from './styles.css?inline';
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -28,11 +25,9 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
       <main>
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
