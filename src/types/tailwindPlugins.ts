dc687e9cf783c4c0235f27bc1274452df5885db2
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-09-26 21:44:14 +0400

    :label: Types tailwind plugins

---
+++ src/types/tailwindPlugins.ts
@@ -0,0 +1,8 @@
+declare module 'tailwindcss-hero-patterns' {
+  const heropatterns: () => { handler: () => void };
+  export default heropatterns;
+}
+declare module 'tailwind-scrollbar-hide' {
+  const scrollbarHide: () => { handler: () => void };
+  export default scrollbarHide;
+}
